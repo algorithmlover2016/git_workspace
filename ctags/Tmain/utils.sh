82a36bc8f900640f933e4f9a5516fd754c86fda1
bo_yu <code_xxgao@163.com>
2016-12-15 15:58:26 +0800

    app to add

---
+++ ctags/Tmain/utils.sh
@@ -0,0 +1,36 @@
+__SKIP__=77
+
+skip()
+{
+	echo "$@"
+	exit ${__SKIP__}
+}
+
+remove_commit_id()
+{
+    # Remove a commit id embedded in tags file
+    sed -i -e '/!_TAG_PROGRAM_VERSION.*/s#/[^/]*/#//#' $1
+}
+
+is_feature_available()
+{
+    local ctags=$1
+    local feat=$2
+
+    if ! ${ctags} --list-features | grep -q "$feat"; then
+		skip "feature \"$feat\" is not available in $ctags"
+    fi
+}
+
+exit_if_no_coproc()
+{
+    is_feature_available $1 coproc
+}
+
+run_with_format()
+{
+    echo '#' $*
+    local format=$1
+    shift
+    ${CTAGS} --quiet --options=NONE --output-format=$format "$@" -o - input.*
+}
